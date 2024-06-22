d77e4658734949932d0d0534f81a06914d07965e
kumar Devashish <kumardevashish000@gmail.com>
2024-06-23 00:32:12 +0530

    refactor: Improve error handling in ErrorHandling contract

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -1,7 +1,12 @@
+// Three Error Handling Function in Solidity 
+//Require
+//Assert
+//Revert
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleGradingSystem {
+contract ErrorHandling {
     address public teacher;
     mapping(address => string) public  grades;
 
@@ -15,26 +20,25 @@ contract SimpleGradingSystem {
         teacher = msg.sender;
     }
 
-    // Function to assign a grade based on marks
+  
     function assignGrade(address student, uint marks) public Teacher {
       
         require(marks >= 0 && marks <= 100, "Marks must be between 0 and 100.");
 
-        // Determine the grade based on the marks
         string memory grade;
         if (marks >= 95) {
             grade = "A+";
-        } else if (marks >= 90) {
+        } else if (95 > marks && marks >= 90) {
             grade = "A";
-        } else if (marks >= 80) {
+        } else if ( 90> marks && marks >= 80) {
             grade = "B+";
-        } else if (marks >= 70) {
+        } else if ( 80> marks &&marks >= 70) {
             grade = "B";
-        } else if (marks >= 60) {
+        } else if (70> marks &&marks >= 60) {
             grade = "C+";
-             } else if (marks >= 50) {
+             } else if (60> marks &&marks >= 50) {
             grade = "C";
-        } else if (marks >= 40) {
+        } else if (50 > marks &&marks >= 40) {
             grade = "D";
         } else {
             grade = "F";
@@ -43,27 +47,26 @@ contract SimpleGradingSystem {
         grades[student] = grade;
     }
 
-    // Function to get the grade of a student
     function getGrade(address student) public view returns (string memory) {
         string memory grade = grades[student];
 
-        // Ensure the grade is not empty
+     
         require(bytes(grade).length > 0, "Student does not have a grade assigned.");
         
         return grade;
     }
 
-    // Function to change the teacher
-    function changeTeacher(address newTeacher) public Teacher {
+  
+    function otherTeacher(address newTeacher) public Teacher {
         // Ensure the new teacher address is not zero
         require(newTeacher != address(0), "New teacher's address must be valid.");
 
         teacher = newTeacher;
     }
 
-    // Function to remove a student's grade
+
     function removeGrade(address student) public Teacher {
-        // Ensure the student has a grade assigned
+     
         string memory grade = grades[student];
         if (bytes(grade).length == 0) {
             revert("Student does not have a grade assigned.");
