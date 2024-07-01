ef9e246ece819e46047732776ef9348c2368c3eb
kumar Devashish <kumardevashish000@gmail.com>
2024-07-01 11:04:45 +0530

    refactor: Improve error handling in ErrorHandling contract

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -1,4 +1,4 @@
-// Three Error Handling Function in Solidity 
+// Three Error Handling Function in Solidity
 //Require
 //Assert
 //Revert
@@ -8,21 +8,21 @@ pragma solidity ^0.8.0;
 
 contract ErrorHandling {
     address public teacher;
-    mapping(address => string) public  grades;
+    mapping(address => string) public grades;
 
     modifier Teacher() {
-        require(msg.sender == teacher, "Only the teacher can Give the grade to the Student.");
+        require(
+            msg.sender == teacher,
+            "Only the teacher can Give the grade to the Student."
+        );
         _;
     }
-
 
     constructor() {
         teacher = msg.sender;
     }
 
-  
     function assignGrade(address student, uint marks) public Teacher {
-      
         require(marks >= 0 && marks <= 100, "Marks must be between 0 and 100.");
 
         string memory grade;
@@ -30,15 +30,15 @@ contract ErrorHandling {
             grade = "A+";
         } else if (95 > marks && marks >= 90) {
             grade = "A";
-        } else if ( 90> marks && marks >= 80) {
+        } else if (90 > marks && marks >= 80) {
             grade = "B+";
-        } else if ( 80> marks &&marks >= 70) {
+        } else if (80 > marks && marks >= 70) {
             grade = "B";
-        } else if (70> marks &&marks >= 60) {
+        } else if (70 > marks && marks >= 60) {
             grade = "C+";
-             } else if (60> marks &&marks >= 50) {
+        } else if (60 > marks && marks >= 50) {
             grade = "C";
-        } else if (50 > marks &&marks >= 40) {
+        } else if (50 > marks && marks >= 40) {
             grade = "D";
         } else {
             grade = "F";
@@ -50,23 +50,25 @@ contract ErrorHandling {
     function getGrade(address student) public view returns (string memory) {
         string memory grade = grades[student];
 
-     
-        require(bytes(grade).length > 0, "Student does not have a grade assigned.");
-        
+        require(
+            bytes(grade).length > 0,
+            "Student does not have a grade assigned."
+        );
+
         return grade;
     }
 
-  
     function otherTeacher(address newTeacher) public Teacher {
         // Ensure the new teacher address is not zero
-        require(newTeacher != address(0), "New teacher's address must be valid.");
+        require(
+            newTeacher != address(0),
+            "New teacher's address must be valid."
+        );
 
         teacher = newTeacher;
     }
 
-
     function removeGrade(address student) public Teacher {
-     
         string memory grade = grades[student];
         if (bytes(grade).length == 0) {
             revert("Student does not have a grade assigned.");
